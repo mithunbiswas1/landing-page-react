7afb96613f16827cb501e09026969a3d75dc2cad
Mithun Biswas <mithunbiswas.me@gmail.com>
2025-03-02 15:56:08 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("#home");
+
+  const navList = [
+    { href: "/#home", label: "Home" },
+    { href: "/#about", label: "About" },
+    { href: "/#services", label: "Services" },
+    { href: "/#contact", label: "Contact" },
+  ];
+
+  return (
+    <div className="w-full fixed top-0 left-0 right-0 backdrop-blur-sm shadow-sm bg-white/90 border border-gray-100">
+      <div className="container w-full flex justify-between items-center gap-4 h-16 md:h-20 px-8 md:px-4">
+        {/* Logos */}
+        <div className="flex justify-center cursor-pointer">
+          <div className="h-4 w-4 bg-blue-600 rounded-full opacity-75 hover:opacity-100 transition-opacity"></div>
+          <div className="-ml-2 h-4 w-4 bg-red-500 rounded-full opacity-100 hover:opacity-75 transition-opacity"></div>
+        </div>
+        {/* Nav */}
+        <div className="hidden md:flex items-center gap-10">
+          {navList.map((links, index) => (
+            <a
+              key={index}
+              href={links.href}
+              onClick={() => setActiveLink(links.href)}
+              className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all ${
+                activeLink === links.href
+                  ? "text-blue-600 after:w-full"
+                  : "text-gray-600 hover:text-gray-900"
+              }`}
+            >
+              {links.label}
+            </a>
+          ))}
+        </div>
+
+        {/* Button */}
+        <button className="hidden md:block bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100">
+          <a href="#newsLetter">Get in touch</a>
+        </button>
+        {/* Moblie Nav */}
+      </div>
+    </div>
+  );
+};

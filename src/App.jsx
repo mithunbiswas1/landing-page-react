d64d03ae5f61feb7a4a551e6046cf6e9fe8245ed
Mithun Biswas <mithunbiswas.me@gmail.com>
2025-03-04 11:09:05 +0600

    logo marquee section running

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { CompanyLogo } from "./components/CompanyLogo";
 import { HeroSection } from "./components/HeroSection";
 import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <HeroSection />
-    </>
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-4 -left-4 w-[800px] h-[600px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <HeroSection />
+        <CompanyLogo />
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/CompanyLogo.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import slack from "../assets/slack.png";
+import amazon from "../assets/amazon.png";
+import woocommerce from "../assets/woocommerce.png";
+import meundies from "../assets/meundies.png";
+import sitepoint from "../assets/sitepoint.png";
+
+export const CompanyLogo = () => {
+  const Logos = [
+    slack,
+    amazon,
+    woocommerce,
+    meundies,
+    sitepoint,
+    slack,
+    amazon,
+    woocommerce,
+    meundies,
+    sitepoint,
+  ];
+  return (
+    <div className="w-full container mx-auto py-20 px-8 sm:px-6 lg:px-8 flex sm:flex-row flex-col sm:items-center items-start">
+      <div className="w-[300px] shrink-0 text-gray-600 px-4 border-l-4 border-blue-500 bg-white py-2 z-10 sm:text-base text-xl font-semibold text-left">
+        Proud partner at <br />
+        Hubspot & Segment
+      </div>
+      <div className="flex whitespace-nowrap gap-8 animate-marquee">
+        {Logos.map((logo, index) => (
+          <img
+            src={logo}
+            alt="company logo"
+            className="mx-12 h-8 w-36 object-contain grayscale hover:grayscale-0 opacity-70 hover:opacity-100"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
